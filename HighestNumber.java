298c7d29519fb28c27928ebce466eb8580c11b0f
ratvim <vimleshraturi@gmail.com>
2016-10-16 12:19:59 +0530

    Update HighestNumber.java

--- HighestNumber.java
+++ HighestNumber.java
@@ -10,21 +10,24 @@ public class HighestNumber {
 		sc = new Scanner(System.in);
 		System.out.println("how many number you want to compare");
 		int a  = sc.nextInt();
-		int [] numbers = new int [a-1];
+//		int [] numbers = new int [a-1];
 		while (a > 0){
 			System.out.println("Enter number:");
 			i = sc.nextInt();
-			numbers[a-1] = i;
+			if (b<=i){
+				b=i;
+			}
+	//		numbers[a-1] = i;
 			a = a-1;
 		}
-	if (numbers[0]>=numbers[1] && numbers[0]>=numbers[2]){
-		b=numbers[0];
-	}else if (numbers[1]>=numbers[0] && numbers[1]>=numbers[2]){
-		b=numbers[1];
+//	if (numbers[0]>=numbers[1] && numbers[0]>=numbers[2]){
+//		b=numbers[0];
+//	}else if (numbers[1]>=numbers[0] && numbers[1]>=numbers[2]){
+//		b=numbers[1];
 		
-	}else if (numbers[2]>=numbers[0]&&numbers[2]>=numbers[1]){
-		b=numbers[2];
-	}	
+//	}else if (numbers[2]>=numbers[0]&&numbers[2]>=numbers[1]){
+//		b=numbers[2];
+//	}	
 	System.out.println("Highest number is: " + b);
 }
 
